f936f83a5908ccc704f42df79304445a8f5ff12d
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-11-26 19:39:57 +0200

    Add files via upload

---
+++ Inheritance/NeedForSpeed/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace NeedForSpeed
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle(300, 100);
+            Vehicle car = new Car(100, 100);
+            Vehicle raceMotorcycle = new RaceMotorcycle(100, 100);
+            Vehicle raceCar = new SportCar(100, 100);
+
+            vehicle.Drive(10);
+            car.Drive(10);
+            raceMotorcycle.Drive(10);
+            raceCar.Drive(10);
+
+            Console.WriteLine(vehicle.Fuel);
+            Console.WriteLine(car.Fuel);
+            Console.WriteLine(raceMotorcycle.Fuel);
+            Console.WriteLine(raceCar.Fuel);
+        }
+    }
+}
