28e76b620fa7e8feb530df2ce9baeab01e0f475e
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-11-26 19:39:20 +0200

    Add files via upload

---
+++ Inheritance/CustomStack/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Stack<string> names = new Stack<string>();
+            names.Push("Gosho");
+            names.Push("Pesho");
+            names.Push("Ivan");
+            names.Push("Dragan");
+            names.Push("Ani");
+
+            Console.WriteLine(stack.IsEmpty());
+
+            stack.AddRange(names);
+
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
