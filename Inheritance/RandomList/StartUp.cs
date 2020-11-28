8e6fed4a3d27061198e78a1304f56413e0f87e9c
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-11-28 20:11:18 +0200

    Add files via upload

---
+++ Inheritance/RandomList/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+            randomList.Add("Gosho");
+            randomList.Add("Pesho");
+            randomList.Add("Ivan");
+            randomList.Add("Dragan");
+            string result = randomList.RandomString();
+
+            Console.WriteLine(result);
+        }
+    }
+}
