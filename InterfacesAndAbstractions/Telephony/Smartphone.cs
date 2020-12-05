28526de6a60328b96f3daa0f7390e94d4651edb7
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-05 16:09:59 +0200

    Add files via upload

---
+++ InterfacesAndAbstractions/Telephony/Smartphone.cs
@@ -0,0 +1,36 @@
+﻿namespace Telephony
+{
+    public class Smartphone : ICallable, IBrowseable
+    {
+        public string Browsing(string site)
+        {
+            for (int i = 0; i < site.Length; i++)
+            {
+                if (char.IsDigit(site[i]))
+                {
+                    return "Invalid URL!";
+                }
+            }
+
+            return $"Browsing: {site}!";
+        }
+
+        public string Calling(string number)
+        {
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (char.IsLetter(number[i]))
+                {
+                    return "Invalid number!";
+                }
+            }
+
+            if (number.Length == 7)
+            {
+                return $"Dialing... {number}";
+            }
+
+            return $"Calling... {number}";
+        }
+    }
+}
---
+++ InterfacesAndAbstractions/Telephony/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] numbers = Console.ReadLine().Split();
+            string[] sites = Console.ReadLine().Split();
+            Smartphone smartphone = new Smartphone();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(smartphone.Calling(number));
+            }
+
+            foreach (var site in sites)
+            {
+                Console.WriteLine(smartphone.Browsing(site));
+            }
+        }
+    }
+}
