aef65e75467838ca974492d70c1ec2b680d1390d
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-05 16:09:29 +0200

    Add files via upload

---
+++ InterfacesAndAbstractions/Shapes/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.Radius = radius;
+        }
+
+        public int Radius
+        {
+            get => this.radius;
+            private set
+            {
+                this.radius = value;
+            }
+        }
+
+        public void Draw()
+        {
+            double radiusIn = this.radius - 0.4;
+            double radiusOut = this.radius + 0.4;
+
+            for (double i = radius; i >= -radius; i--)
+            {
+                for (double j = -radius; j < radiusOut; j += 0.5)
+                {
+                    double value = j * j + i * i;
+
+                    if(value >= radiusIn * radiusIn && value <= radiusOut * radiusOut)
+                    {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ InterfacesAndAbstractions/Shapes/Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public int Width
+        {
+            get => this.width;
+            private set
+            {
+                this.width = value;
+            }
+        }
+
+        public int Height
+        {
+            get => this.height;
+            private set
+            {
+                this.height = value;
+            }
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+
+            for (int i = 1; i < height - 1; i++)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+
+            DrawLine(this.width, '*', '*');
+        }
+
+        public void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+
+            for (int i = 1; i < width - 1; i++)
+            {
+                Console.Write(mid);
+            }
+
+            Console.WriteLine(end);
+        }
+    }
+}
