aca5f696b3ef8410280557e1f7c891637c3e44fc
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-05 16:08:53 +0200

    Add files via upload

---
+++ InterfacesAndAbstractions/PersonInfo/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+
+            IIdentifiable person = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
+            Console.WriteLine(person.Id);
+            Console.WriteLine(birthable.Birthdate);
+        }
+    }
+}
