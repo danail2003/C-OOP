bad31deb2b094ad949336571d884b855b6f669fd
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-07 19:01:25 +0200

    Add files via upload

---
+++ Encapsulation/ClassBoxData/Box.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private decimal length;
+        private decimal width;
+        private decimal height;
+
+        public Box(decimal length, decimal width, decimal height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public decimal Length
+        {
+            get => this.length;
+            private set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        public decimal Width
+        {
+            get => this.width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public decimal Height
+        {
+            get => this.height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public decimal SurfaceArea(decimal length, decimal width, decimal height)
+        {
+            decimal surfaceArea = (2 * length * width) + (2 * length * height) + (2 * width * height);
+
+            return surfaceArea;
+        }
+
+        public decimal LateralSurfaceArea(decimal length, decimal width, decimal height)
+        {
+            decimal lateralArea = (2 * length * height) + (2 * width * height);
+
+            return lateralArea;
+        }
+
+        public decimal Volume(decimal length, decimal width, decimal height)
+        {
+            decimal volume = length * width * height;
+
+            return volume;
+        }
+    }
+}
---
+++ Encapsulation/ClassBoxData/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            try
+            {
+                decimal length = decimal.Parse(Console.ReadLine());
+                decimal width = decimal.Parse(Console.ReadLine());
+                decimal height = decimal.Parse(Console.ReadLine());
+
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.SurfaceArea(length, width, height):f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea(length, width, height):f2}");
+                Console.WriteLine($"Volume - {box.Volume(length, width, height):f2}");
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
