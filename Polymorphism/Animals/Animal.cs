dc65b0d1f1dc48b5990f0f19c238f1e19f7b96bc
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-09 20:06:43 +0200

    Add files via upload

---
+++ Polymorphism/Animals/Animal.cs
@@ -0,0 +1,32 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set { this.name = value; }
+        }
+
+        public string FavouriteFood
+        {
+            get => this.favouriteFood;
+            private set { this.favouriteFood = value; }
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.name} and my fovourite food is {this.favouriteFood}";
+        }
+    }
+}
