4c3e405cbb19febdc27aac94c1d1f1c7cbd27290
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-09 20:03:14 +0200

    Add files via upload

---
+++ Encapsulation/PersonsInfo/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] token = Console.ReadLine().Split();
+
+                people.Add(new Person(token[0], token[1], int.Parse(token[2]), decimal.Parse(token[3])));
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (var person in people)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
