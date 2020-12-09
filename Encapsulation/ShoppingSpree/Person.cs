0badf5adbe9e2351e4385a2dfcb476a35b10cbcb
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-09 20:04:10 +0200

    Add files via upload

---
+++ Encapsulation/ShoppingSpree/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bagOfProducts;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bagOfProducts = new List<Product>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get => this.money;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            this.bagOfProducts.Add(product);
+        }
+
+        public List<Product> CountOfProducts()
+        {
+            return this.bagOfProducts;
+        }
+    }
+}
---
+++ Encapsulation/ShoppingSpree/StartUp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] people = Console.ReadLine().Split(new char[] { '=', ';'});
+            string[] products = Console.ReadLine().Split(new char[] { '=', ';'});
+            string command = Console.ReadLine();
+            List<Person> listOfPeople = new List<Person>();
+            List<Product> listOfProducts = new List<Product>();
+
+            for (int i = 0; i < people.Length; i += 2)
+            {
+                try
+                {
+                    listOfPeople.Add(new Person(people[i], decimal.Parse(people[i + 1])));
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+
+            for (int i = 0; i < products.Length; i += 2)
+            {
+                try
+                {
+                    listOfProducts.Add(new Product(products[i], decimal.Parse(products[i + 1])));
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+
+            while (command != "END")
+            {
+                string[] token = command.Split();
+
+                Person person = listOfPeople.FirstOrDefault(x => x.Name == token[0]);
+                Product product = listOfProducts.FirstOrDefault(x => x.Name == token[1]);
+
+                if (person.Money >= product.Cost)
+                {
+                    person.Money -= product.Cost;
+                    person.AddProduct(product);
+                    Console.WriteLine($"{person.Name} bought {product.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name} can't afford {product.Name}");
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var person in listOfPeople)
+            {
+                if (person.CountOfProducts().Count > 0)
+                {
+                    Console.WriteLine($"{person.Name} - {string.Join(", ", person.CountOfProducts().Select(x => x.Name))}");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                }
+            }
+        }
+    }
+}
