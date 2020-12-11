bfd9c3b0241638ef6df517440197adc6376b819b
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-11 17:08:33 +0200

    Add files via upload

---
+++ Reflection And Attributes/CommandPattern/Models/CommandInterpreter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Models
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string command = "Command";
+
+        public string Read(string args)
+        {
+            string[] token = args.Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
+            string[] commands = token.Skip(1).ToArray();
+
+            string commandName = token[0] + command;
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type type = assembly.GetTypes().FirstOrDefault(x => x.Name.ToLower() == commandName.ToLower());
+
+            if (type == null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+
+            ICommand concreteCommand = (ICommand)Activator.CreateInstance(type);
+
+            string result = concreteCommand.Execute(commands);
+
+            return result;
+        }
+    }
+}
